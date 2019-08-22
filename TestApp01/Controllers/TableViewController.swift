0311b19a399c67b062d586a14c6f623bde001957
AngelaLiForever <AngelaLiForever@gmail.com>
2019-08-22 16:17:35 -0700

    Convert to MVC design pattern

--- TestApp01/Controllers/TableViewController.swift
+++ TestApp01/Controllers/TableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TableViewController: UITableViewController{
 
-    var itemArray = ["Hello World","How Are You","I Am Fine"]
+    var itemArray = [Item]()
     
     var clickedCellText = ""
     
@@ -22,13 +22,29 @@ class TableViewController: UITableViewController{
         
         
         //Angela: In case the default is empty, if not empty store the default item to Array
-//        if let items = defaults.array(forKey: "itemArray") as? [String] {
-//            itemArray = items
-//        }
-//
-        if let cellText = defaults.string(forKey: "clickedCellText") {
-           clickedCellText = cellText
+        
+        let newItem0 = Item()
+        newItem0.title = "Hello World"
+        itemArray.append(newItem0)
+
+        let newItem1 = Item()
+        newItem1.title = "How Are You"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "I Am Fine"
+        itemArray.append(newItem2)
+        
+        
+        
+        
+        if let items = defaults.array(forKey: "itemArray") as? [Item] {
+            itemArray = items
         }
+
+//        if let cellText = defaults.string(forKey: "clickedCellText") {
+//           clickedCellText = cellText
+//        }
         
     }
 
@@ -37,9 +53,11 @@ class TableViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
         
         return cell
         
@@ -48,12 +66,12 @@ class TableViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
        // self.defaults.set(self.itemArray, forKey: "itemArray")
-        self.defaults.set(self.itemArray[indexPath.row], forKey: "clickedCellText")
-        
-        let defaultCelltext = defaults.object(forKey:"clickedCellText")
+//        self.defaults.set(self.itemArray[indexPath.row], forKey: "clickedCellText")
+//
+//        let defaultCelltext = defaults.object(forKey:"clickedCellText")
         
         print("hi")
-        print(defaultCelltext ?? "fail to display default cell contents")
+//        print(defaultCelltext ?? "fail to display default cell contents")
 
     }
     
@@ -68,9 +86,14 @@ class TableViewController: UITableViewController{
         let action = UIAlertAction(title: "Display", style: .default) { (action) in
             //what will happen onece the user clicks the display button on our UIAlert
             //Add the added field in the Array
-            self.itemArray.append(textField.text ?? "New Item")
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "itemArray")
+            
             
             //This is to load the entered data and display in the cell
             self.tableView.reloadData()
